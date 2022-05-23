bd2771da0ca7a0544bd06b17142adea1e7ca43ab
Oleksii Diachenko <alexey.dyachenko@ukr.net>
2022-05-23 18:01:09 +0300

    version 1.01

--- application.py
+++ application.py
@@ -106,8 +106,8 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
-    <p>Your(Oleksii) first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <h1 color=blue>Congratulations Oleksii</h1>
+    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
   
